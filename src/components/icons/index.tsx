366a07e51f1c8114f2cf7be292334f0d0276bdcd
Abhishek Kumar <github.abhiarya@gmail.com>
2025-06-28 11:38:10 +0530

    added: header-section

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,48 @@
+import { LucideMail, LucidePhone, LucideProps } from "lucide-react";
+
+export const Icons = {
+	github: (props: LucideProps) => (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+			<path
+				fill="currentColor"
+				d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
+			></path>
+		</svg>
+	),
+	email: LucideMail,
+	phone: LucidePhone,
+	x: (props: LucideProps) => (
+		<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+			<path
+				fill="currentColor"
+				d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
+			></path>
+		</svg>
+	),
+	linkedin: (props: LucideProps) => (
+		<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+			<path
+				fill="currentColor"
+				d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"
+			></path>
+		</svg>
+	),
+	resume: (props: LucideProps) => (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			{...props}
+		>
+			<rect x="3" y="2" width="18" height="20" rx="2"></rect>
+			<path d="M3 8h18"></path>
+			<path d="M3 12h10"></path>
+			<path d="M3 16h10"></path>
+			<path d="M6 4h8v2H6z"></path>
+		</svg>
+	),
+};
