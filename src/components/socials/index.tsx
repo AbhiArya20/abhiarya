a415aadc16b25c7122a320ff035b1d6b8127b08d
Abhishek Kumar <github.abhiarya@gmail.com>
2025-06-30 12:45:35 +0530

    added: social-component

--- src/components/socials/index.tsx
+++ src/components/socials/index.tsx
@@ -3,6 +3,6 @@ import SocialV1 from "@/components/socials/social-v1";
 export default function SocialComponent() {
   const socials = [SocialV1];
   const randomIndex = Math.floor(Math.random() * socials.length);
-  const RandomHeader = socials[randomIndex];
-  return <RandomHeader />;
+  const RandomSocial = socials[randomIndex];
+  return <RandomSocial />;
 }
