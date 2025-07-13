fc35b1596109a3d79cc74726ee792cd9fe6d8f3f
Abhishek Kumar <github.abhiarya@gmail.com>
2025-07-13 10:07:10 +0530

    fixed: /app

--- src/components/sections/header/index.tsx
+++ src/components/sections/header/index.tsx
@@ -3,6 +3,6 @@ import HeaderV1 from "@/components/sections/header/header-v1";
 
 export default function Header() {
   const headers = [HeaderV1, HeaderV2];
-  const SelectedHeader = headers[1];
+  const SelectedHeader = headers[0];
   return <SelectedHeader />;
 }
