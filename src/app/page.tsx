a0020c62ce3b3ddc959afc742de41715e8083168
Abhishek Kumar <github.abhiarya@gmail.com>
2025-07-31 02:02:07 +0530

    /app/** is cleaned-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import HomePageV1 from "@/components/home-page/home-page-v1";
+import HomePageV1 from "@/components/home-pages/home-page-v1";
 
 export default function Home() {
   return <HomePageV1 />;
