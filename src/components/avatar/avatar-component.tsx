65994975f85235d50b67d449e5e97d96b16ffb7c
Abhishek Kumar <github.abhiarya@gmail.com>
2025-08-15 09:52:49 +0530

    fixed: avatar flashing issue

--- src/components/avatar/avatar-component.tsx
+++ src/components/avatar/avatar-component.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { PERSONAL_DATA } from "@/data/personal";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export default function AvatarComponent({ className }: { className?: string }) {
   return (
@@ -10,7 +11,19 @@ export default function AvatarComponent({ className }: { className?: string }) {
         src={PERSONAL_DATA.githubAvatar || PERSONAL_DATA.avatar}
         alt={PERSONAL_DATA.nickname}
       ></AvatarImage>
-      <AvatarFallback className="text-3xl">{PERSONAL_DATA.nickname.charAt(0)}</AvatarFallback>
+      <AvatarFallback className="overflow-hidden rounded-xl text-3xl">
+        {/* Image as a fallback because Image component support caching and blur-hash */}
+        <Image
+          src={PERSONAL_DATA.githubAvatar || PERSONAL_DATA.avatar}
+          alt={PERSONAL_DATA.nickname}
+          width={300}
+          height={300}
+          placeholder="blur"
+          blurDataURL="e6EfTi00~qM{_3_3-;xu4n9F00M{j[IUM{~qD%9F-;xu%MofRjxuD%"
+          className="h-full w-full object-cover"
+          priority
+        />
+      </AvatarFallback>
     </Avatar>
   );
 }
