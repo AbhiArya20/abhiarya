853e699c4f220511311ea41de67eb2ab754c3d87
Abhishek Kumar <github.abhiarya@gmail.com>
2025-10-04 12:29:45 +0530

    added: one-ko

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "@/app/globals.css";
 
+import OneKo from "@/components/one-ko/one-ko";
 import ThemeProvider from "@/components/providers/theme-provider";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} bg-background min-h-screen font-sans antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
+          <OneKo />
         </ThemeProvider>
         <Analytics />
         <SpeedInsights />
