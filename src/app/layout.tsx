38300a4f4dac81d389ec2a52fe2098c447ebce83
Abhishek Kumar <github.abhiarya@gmail.com>
2025-10-16 16:13:46 +0530

    Added OneKo components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import OneKo from "@/components/oneko/oneko";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 import "@/app/globals.css";
@@ -38,6 +39,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <OneKo />
         </ThemeProvider>
         <SpeedInsights />
         <Analytics />
