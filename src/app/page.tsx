5d8640066f19031237b0dee6a3970645a3e1cf85
Abhishek Kumar <github.abhiarya@gmail.com>
2025-10-17 23:31:30 +0530

    Added: personal data, components(Avatar, Badge)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+import AvatarComponent from "@/components/avatar/avatar-component";
+import Badges from "@/components/badges/badges";
 import Social from "@/components/socials/social";
 import ThemeSwitcher from "@/components/theme-switcher/theme-switcher";
+import { PERSONAL_DATA } from "@/data/personal";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen min-w-full justify-center items-center flex-col gap-4">
       <ThemeSwitcher />
       <Social />
+      <AvatarComponent />
+      <Badges name="React" icon="react" url="https://reactjs.org" />
+      <div className="">{PERSONAL_DATA.about}</div>
     </div>
   );
 }
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+import type { Icons } from "@/components/icons";
+
+export type Tag = {
+  name: string;
+  icon?: Icons;
+  url?: string;
+};
