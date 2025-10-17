7266d00992c866b5b5e945db8f3b6fa968e181a5
Abhishek Kumar <github.abhiarya@gmail.com>
2025-10-18 04:18:03 +0530

    Added: open-source section

---
+++ src/components/error/error-card.tsx
@@ -0,0 +1,26 @@
+import type React from "react";
+import { cn } from "@/lib/utils";
+
+export default function ErrorCard({
+  message,
+  icon,
+  className,
+}: {
+  message: string;
+  icon: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className="border-border rounded-lg border-2 p-4 shadow-sm">
+      <div
+        className={cn(
+          "flex h-full items-center justify-center gap-4 py-16",
+          className,
+        )}
+      >
+        {icon}
+        <span>{message}</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/open-source/pull-request.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { GitPullRequestArrow, WifiOff } from "lucide-react";
+import { motion } from "motion/react";
+import ErrorCard from "@/components/error/error-card";
+import PullRequestCard from "@/components/open-source/pull-request-card";
+import { fadeDownChildVariants } from "@/lib/animation-variants";
+import type { ActionsReturn, PullRequest } from "@/types";
+
+export default function PullRequestComponent({
+  pullRequest,
+}: {
+  pullRequest: ActionsReturn<PullRequest[]>;
+}) {
+  const { data, error } = pullRequest;
+
+  return (
+    <>
+      <motion.div variants={fadeDownChildVariants}>
+        {error && (
+          <ErrorCard
+            message={error.message}
+            icon={<WifiOff className="size-5" />}
+            className="text-destructive"
+          />
+        )}
+      </motion.div>
+
+      {data &&
+        data.length > 0 &&
+        data?.map((pr) => (
+          <PullRequestCard
+            key={pr.id}
+            id={pr.id}
+            title={pr.title}
+            url={pr.url}
+            createdAt={pr.createdAt}
+            state={pr.state}
+            deletions={pr.deletions}
+            additions={pr.additions}
+            number={pr.number}
+            commits={pr.commits}
+            changedFiles={pr.changedFiles}
+            closedAt={pr.closedAt}
+            mergedAt={pr.mergedAt}
+            author={pr.author}
+            repository={pr.repository}
+          />
+        ))}
+
+      <motion.div variants={fadeDownChildVariants}>
+        {data && data.length === 0 && (
+          <ErrorCard
+            message={"No Pull Requests found..."}
+            icon={<GitPullRequestArrow className="size-5" />}
+            className="text-muted-foreground"
+          />
+        )}
+      </motion.div>
+    </>
+  );
+}
